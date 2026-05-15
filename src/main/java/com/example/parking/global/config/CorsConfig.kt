5c5d4f362a2251ecf019b24992a2bb0f62685558
sorbet0404 <151494073+sorbet0404@users.noreply.github.com>
2026-05-15 10:21:29 +0900

    refactor: Global Config Kotlin 전환 및 QueryDSL 설정 추가 (#18) (#24)

---
+++ src/main/java/com/example/parking/global/config/CorsConfig.kt
@@ -0,0 +1,24 @@
+package com.example.parking.global.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+
+@Configuration
+class CorsConfig {
+
+    @Bean
+    fun corsConfigurationSource(): CorsConfigurationSource {
+        val config = CorsConfiguration()
+        config.allowedOrigins = listOf("http://localhost:3000")
+        config.allowedMethods = listOf("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+        config.allowedHeaders = listOf("*")
+        config.allowCredentials = true
+
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", config)
+        return source
+    }
+}
