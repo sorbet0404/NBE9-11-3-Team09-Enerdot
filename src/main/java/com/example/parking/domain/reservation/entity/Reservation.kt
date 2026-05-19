5c322e93de7716a465692977fb7d16f2fbfdee0a
sorbet0404 <151494073+sorbet0404@users.noreply.github.com>
2026-05-19 09:44:15 +0900

    bug: 출차완료되도 동일 주차장 예약 불가 버그 해결 (#83)

--- src/main/java/com/example/parking/domain/reservation/entity/Reservation.kt
+++ src/main/java/com/example/parking/domain/reservation/entity/Reservation.kt
@@ -73,6 +73,10 @@ class Reservation protected constructor(
         this.status = ReservationStatus.COMPLETED
     }
 
+    fun finish() {
+        this.status = ReservationStatus.FINISHED
+    }
+
     fun pending() {
         this.status = ReservationStatus.PENDING
     }
